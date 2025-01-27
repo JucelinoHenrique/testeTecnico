fb9ab35a490b96781d9fb3d2dde97c1052fd25a1
JucelinoHenrique <102430350+JucelinoHenrique@users.noreply.github.com>
2025-01-27 14:57:09 -0300

    adicionado o cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes';
 
 const app = express();
 
+// Configuração do CORS
+app.use(cors()); 
+
+
 app.use(bodyParser.json());
+
 app.use('/api/users', userRoutes);
 
 export default app;
-    
